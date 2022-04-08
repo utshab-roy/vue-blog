f8af77c1c46617386711471d1926b34d6f43aa48
Utshab Roy <utshab.roy@gmail.com>
2022-04-08 16:25:26 +0900

    start file setup for workout tracker app with vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-    <header>
-        <div class="wrapper">
-            <nav>
-                <RouterLink to="/">Home</RouterLink> ||
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-        </div>
-    </header>
-
-    <RouterView />
+  <div>
+    <router-view />
+  </div>
 </template>
 
-<style></style>
+<script>
+export default {
+  setup() {
+    // Create data / vars
+
+    // Check to see if user is already logged in
+
+    // If user does not exist, need to make app ready
+
+    // Runs when there is a auth state change
+    // if user is logged in, this will fire
+
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap");
+</style>
