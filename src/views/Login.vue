6c1094f064e892798a6d465524133288ac829c46
Utshab Roy <utshab.roy@gmail.com>
2022-04-09 13:37:00 +0900

    login func impl

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,6 +7,7 @@
 
     <!-- login from -->
     <form
+      @submit.prevent="login"
       class="p-8 flex flex-col bg-light-grey rounded-md bg-light-gray shadow-lg"
     >
       <h1 class="text-3xl text-at-light-green mb-4">Login</h1>
@@ -54,17 +55,35 @@
 
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
+
 export default {
   name: "login",
   setup() {
     // Create data / vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const errorMsg = ref(null);
 
     // Login function
-
-    return { email, password, errorMsg };
+    const login = async () => {
+      try {
+        const { error } = await supabase.auth.signIn({
+          email: email.value,
+          password: password.value,
+        });
+        if (error) throw error;
+        router.push({ name: "Home" });
+      } catch (error) {
+        errorMsg.value = `Error: ${error.message}`;
+        setTimeout(() => {
+          errorMsg.value = null;
+        }, 5000);
+      }
+    };
+    return { email, password, errorMsg, login };
   },
 };
 </script>
