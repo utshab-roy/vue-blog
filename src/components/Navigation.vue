66f4f5e88fe9681d45179016ac78d0482834546d
Utshab Roy <utshab.roy@gmail.com>
2022-04-09 19:38:37 +0900

    state management with composition api set user data, logout func impl

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,32 +1,45 @@
 <template>
   <header class="bg-at-light-green text-white">
-
-    <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+    <nav
+      class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row"
+    >
       <div class="flex items-center gap-x-4">
-        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
         <h1 class="text-lg">Active Tracker</h1>
       </div>
       <ul class="flex flex-1 justify-end gap-x-10">
-        <router-link class="cursor-pointer" :to="{name: 'Home'}">Home</router-link>
-        <router-link class="cursor-pointer" :to="{name: 'Register'}">Create</router-link>
-        <router-link class="cursor-pointer" :to="{name: 'Login'}">Login</router-link>
-        <li class="cursor-pointer">Logout</li>
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }"
+          >Home</router-link
+        >
+        <router-link class="cursor-pointer" :to="{ name: 'Register' }"
+          >Create</router-link
+        >
+        <router-link class="cursor-pointer" :to="{ name: 'Login' }"
+          >Login</router-link
+        >
+        <li @click="logout" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
-
   </header>
 </template>
 
 <script>
+import { useRoute } from "vue-router";
+import { supabase } from "../supabase/init";
+
 export default {
   setup() {
     // Get user from store
 
     // Setup ref to router
+    const router = useRoute();
 
     // Logout function
-
-    return {};
+    const logout = async () => {
+      await supabase.auth.signOut();
+      router.push({ name: "Home" });
+    };
+    return { logout };
   },
 };
 </script>
