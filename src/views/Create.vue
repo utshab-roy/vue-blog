aec19b74d3d55dfd5ce9ab2988dd60c023ad41ac
Utshab Roy <utshab.roy@gmail.com>
2022-04-12 12:08:31 +0900

    create page layout completed

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -120,6 +120,83 @@
             Add Exercise
           </button>
         </div>
+
+        <!-- Cardio Training Inputs -->
+        <div v-if="workoutType === 'cardio'" class="flex flex-col gap-y-4">
+          <div
+            class="flex flex-col gap-x-6 gap-y-2 relative md:flex-row"
+            v-for="(item, index) in exercises"
+            :key="index"
+          >
+            <div class="flex flex-col md:w-1/3">
+              <label for="cardio-type" class="mb-1 text-sm text-at-green"
+                >Type</label
+              >
+              <select
+                id="cardio-type"
+                class="p-2 w-full text-gray-500 focus:outline-none"
+                v-model="item.cardioType"
+              >
+                <option value="#">Select type</option>
+                <option value="run">Run</option>
+                <option value="walk">Walk</option>
+              </select>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="distance" class="mb-1 text-sm text-at-green">Distance</label>
+              <input
+                type="text"
+                required
+                class="p-2 w-full text-grey-500 focus:outline-none"
+                v-model="item.distance"
+              />
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="duration" class="mb-1 text-sm text-at-green">Duration</label>
+              <input
+                type="text"
+                required
+                class="p-2 w-full text-grey-500 focus:outline-none"
+                v-model="item.duration"
+              />
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="pace" class="mb-1 text-sm text-at-green"
+                >Pace</label
+              >
+              <input
+                type="text"
+                required
+                class="p-2 w-full text-grey-500 focus:outline-none"
+                v-model="item.pace"
+              />
+            </div>
+            <img
+              src="@/assets/images/trash-light-green.png"
+              class="h-4 w-auto absolute -left-5 cursor-pointer"
+              alt=""
+            />
+          </div>
+          <button
+            type="button"
+            class="
+              mt-6
+              py-2
+              px-6
+              rounded-sm
+              self-start
+              text-sm text-white
+              bg-at-light-green
+              duration-200
+              border-solid border-2 border-transparent
+              hover:border-at-light-green
+              hover:bg-white
+              hover:text-at-light-green
+            "
+          >
+            Add Exercise
+          </button>
+        </div>
       </form>
     </div>
   </div>
